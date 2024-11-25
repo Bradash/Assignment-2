60a873520214b16acb443d88a2239220d10b647e
Brandon <brandonhutchison6@gmail.com>
2024-11-25 01:55:07 -0500

    Journal #7 - Jump Mechanics
    Help me this took so long.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -11,6 +13,17 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed;
     public float accelerationTime;
     private float accelerationRate;
+    public float jumpStrength;
+    public float jump;
+    public float jumpTime;
+    public float apexHeight;
+    public float apexTime;
+    private float apexRate;
+    public float airControl;
+    public float terminalSpeed;
+    public float terminalTime;
+    public float coyoteTime;
+    public float coyoteTimer;
 
     public enum FacingDirection
     {
@@ -21,12 +34,38 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         accelerationRate = maxSpeed / accelerationTime;
+        apexRate = apexHeight / apexTime;
     }
+    private void Update()
+    { 
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        if (BoolGrounded == false && coyoteTimer > 0)
+        {
+            coyoteTimer -= Time.deltaTime;
+        }
+        if (coyoteTimer <= 0 & BoolGrounded == false)
+        {
+            rb.gravityScale += terminalSpeed / terminalTime * Time.deltaTime;
+        }
+            if (BoolGrounded == true)
+        {
+            rb.gravityScale = 1;
+            coyoteTimer = coyoteTime;
+        }
+        if (Input.GetButtonDown("Jump") && coyoteTimer > 0)
+        {
+            jump = 1 * jumpStrength;
+        }
+        if (Input.GetButton("Jump"))
+        {
+            jump -= jumpTime * Time.deltaTime;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            jump = 0 * jumpStrength;
+            coyoteTimer = 0;
+        }
+        
     }
     private void FixedUpdate()
     {
@@ -34,13 +73,24 @@ public class PlayerController : MonoBehaviour
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
-        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), jump);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        rb.velocity += playerInput * accelerationRate * Time.deltaTime;
+        Vector2 movement;
+        if (BoolGrounded == true)
+        {
+            movement.x = playerInput.x * accelerationRate;
+        }
+        else
+        {
+            movement.x = playerInput.x * accelerationRate / airControl;
+        }
+        
+        movement.y = playerInput.y * apexRate;
+        rb.velocity += movement * Time.deltaTime;
     }
 
     public bool IsWalking()
@@ -65,18 +115,20 @@ public class PlayerController : MonoBehaviour
     }
     void OnCollisionExit2D(Collision2D collision)
     {
-            BoolGrounded = false;
+        BoolGrounded = false;
     }
+
+
 
     public bool IsGrounded()
     {
         if (BoolGrounded == true)
         {
-            return false;
+            return true;
         }
         else
         {
-            return true;
+            return false;
         }
     }
         
